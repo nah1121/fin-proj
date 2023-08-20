306318db53ebbd54b9174def2c024e41e547a8d3
nah1121 <nahomyeshitla1121@gmail.com>
2023-08-20 17:17:47 +0300

    updated

--- config/config.php
+++ config/config.php
@@ -1,15 +1,25 @@
 <?php
 
-    $hostname = "ep-rough-union-44345254-pooler.us-east-1.postgres.vercel-storage.com";
-    $username = "default";
-    $password = "d21ojFXVbAah";
+$connectionUrl = getenv("postgres://default:d21ojFXVbAah@ep-rough-union-44345254-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb");
+$username = getenv("default");
+$password = getenv("d21ojFXVbAah");
+
+try {
+   
+
+    // Extract host, port, and dbname from the parsed URL
+    $host = "ep-rough-union-44345254-pooler.us-east-1.postgres.vercel-storage.com";
+    $port = "5432";
     $dbname = "verceldb";
 
-    try {
-        $conn = new PDO("pgsql:host=$hostname;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
-    }
+    // Build the DSN for PDO
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
 
+    // Establish the PDO connection
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
     ?>
